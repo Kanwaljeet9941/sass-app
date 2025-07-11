99cd9e4fa1d883a91a05dde4a9b9ea3fd9134767
Kanwaljeet Rathore <kanwaljee@centocode.com>
2025-07-11 16:27:52 +0530

    Implement Auth, Billing, Database Setup, Create Companion Server action

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,16 @@
 import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 import "./globals.css";
-import Navbar from "@/app/companions/Navbar";
+import Navbar from "@/components/Navbar";
+import React from "react";
 
 const bricolage = Bricolage_Grotesque({
   variable: "--font-bricolage",
@@ -19,8 +28,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${bricolage.variable} antialiased`}><Navbar/>{children}</body>
-    </html>
+      <html lang="en">
+        <body className={`${bricolage.variable} antialiased`}>
+          <ClerkProvider appearance={{variables:{colorPrimary: '#fe5933'}}}>
+            {/*<header>*/}
+            {/*  <SignedOut>*/}
+            {/*    <SignInButton />*/}
+            {/*    <SignUpButton />*/}
+            {/*  </SignedOut>*/}
+            {/*  <SignedIn>*/}
+            {/*    <UserButton />*/}
+            {/*  </SignedIn>*/}
+            {/*</header>*/}
+            <Navbar/>
+            {children}
+          </ClerkProvider>
+        </body>
+      </html>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import Link from "next/link";
 import Image from "next/image";
-import Navitems from "@/app/companions/Navitems";
+import Navitems from "@/components/Navitems";
+import {SignedOut,SignedIn,SignInButton,UserButton} from "@clerk/nextjs";
+
+
 
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <Link href='/'>
+            <Link href='/public'>
                 <div className="flex items-center gap-2.5 cursor-pointer"><Image src="/images/logo.svg" alt="logo" width={46} height={46}/></div>
             </Link>
             <div className="flex items-center gap-8">
                 <Navitems/>
-                <p>Sign In</p>
+                <SignedOut>
+                    <SignInButton>
+                        <button className="btn-signin">Sign In</button>
+                    </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
             </div>
         </nav>
     );
